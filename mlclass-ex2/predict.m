2afe29938400f7cb4634302035b7197c8c97c8ae
Sharipov Ruslan <sharipov.ru@gmail.com>
2013-11-26 01:14:23 +0700

    week 3 - predict function

--- mlclass-ex2/predict.m
+++ mlclass-ex2/predict.m
@@ -15,10 +15,17 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+% Evaluate probabilities for every student
+probabilities = sigmoid(X * theta)
 
-
-
-
+% Decide whether students are admitted or not
+for i=1:size(probabilities)
+  if (probabilities(i) >= 0.5) == 1,
+    p(i) = 1
+  else
+    p(i) = 0
+  end
+end
 
 
 % =========================================================================
