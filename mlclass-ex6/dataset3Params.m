a90274bfa655b53f71881fa9fc15e99f0dce7169
Sharipov Ruslan <sharipov.ru@gmail.com>
2013-12-15 18:39:30 +0700

    week 7 - programatically determine best SVM parameters (C, sigma)

--- mlclass-ex6/dataset3Params.m
+++ mlclass-ex6/dataset3Params.m
@@ -23,6 +23,28 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+possible_c     = [0.01 0.03 0.1 0.3 1 3 10 30]
+possible_sigma = [0.01 0.03 0.1 0.3 1 3 10 30]
+current_min_error = 100000
+
+for i=1:size(possible_c, 2)
+  for j=1:size(possible_sigma, 2)
+    iteration = i + j
+
+    model= svmTrain(X, y, possible_c(i), @(x1, x2) gaussianKernel(x1, x2, possible_sigma(j)));
+    visualizeBoundary(X, y, model);
+
+    predictions = svmPredict(model, Xval);
+    err = mean(double(predictions ~= yval))
+
+    if err < current_min_error
+      current_min_error = err
+      C = possible_c(i)
+      sigma = possible_sigma(j)
+    end
+
+  end
+end
 
 
 
